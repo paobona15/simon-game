c76c0af53e4ecbedfa244fedea37a732bf3a43f4
Paolo Bonaobra <162660426+paobona15@users.noreply.github.com>
2024-04-21 00:01:49 +0800

    added sounds when a color is pressed, revised math.round to math.floor

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 var buttons = [];
-$("body").click(function(event) {
+var playerButtons = [];
+$("body").keypress(function(event) {
     gamePattern();
-    console.log(event.target);
     console.log(buttons);
+    console.log(event.target);
 }
 )
 function gameStart() {
 
 }
 function gamePattern() {
-    var colorPattern = Math.round(Math.random()*3)+1;
-    buttons.push(document.querySelectorAll(".btn")[x-1]);
+    var colorPattern = Math.floor(Math.random()*4)+1;
+    buttons.push(colorPattern);
     for (var x=1; x<=4; x++) {
     if (colorPattern===x) {
     $(".btn").eq(x-1).animate({opacity: 0.5});
+    setTimeout($(".btn").eq(x-1).animate({opacity: 1}), 1000)
     }
-
- 
-    //setTimeout(function (), 1000);
+    switch (colorPattern) {
+        case 1:
+            var audio = new Audio("./sounds/green.mp3");
+            audio.play();
+            break;
+        case 2:
+            var audio = new Audio("./sounds/red.mp3");
+            audio.play();
+            break;
+        case 3:
+            var audio = new Audio("./sounds/yellow.mp3");
+            audio.play();
+            break;
+        case 4:
+            var audio = new Audio("./sounds/blue.mp3");
+            audio.play();
+            break;
+        default:
+            break;
+    }
 }
 }
